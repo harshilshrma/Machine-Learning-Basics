c9a9a771bf34f479bbda05972ea2f748022c39d4
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-02-04 05:42:30 +0530

    Imports done

---
+++ 01_BostonHousing/bostonHousing.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+# Assuming X contains 'RM' feature and y contains the target variable 'MEDV'
+# rm: Average number of rooms per dwelling & medv: Median value of owner-occupied homes in $1000
+
+
