cce23630d86215dd935b1b8c9c5ddeda04f39568
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-02-04 07:45:15 +0530

    basic code done

--- 01_BostonHousing/bostonHousing.py
+++ 01_BostonHousing/bostonHousing.py
@@ -1,10 +1,30 @@
 import numpy as np
+import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-# Assuming X contains 'RM' feature and y contains the target variable 'MEDV'
-# rm: Average number of rooms per dwelling & medv: Median value of owner-occupied homes in $1000
+# Loading the dataset from the CSV file
+file_path = 'BostonHousing.csv'
+data = pd.read_csv(file_path)
 
+# Assuming X contains 'RM' feature and y contains the target variable 'MEDV' where,
+#   rm: Average number of rooms per dwelling &
+#   medv: Median value of owner-occupied homes
+X = np.array(data['rm']).reshape(-1, 1)
+y = np.array(data['medv'])
 
+# Splitting the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initializing and fitting the model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Making predictions on the test set
+y_pred = model.predict(X_test)
+
+# Evaluating the model by mean squared error
+mse = mean_squared_error(y_test, y_pred)
+print(f'Mean square error: {mse}')
